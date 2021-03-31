ad9694134bf41e3e42574eeddeae6b101014a13e
Jordi Keijzers <jordi.keijzers@rocdeleijgraaf.nl>
2021-03-31 14:02:24 +0200

    Add login form

--- login.php
+++ login.php
@@ -6,5 +6,18 @@
 ?>
 
                 <h2>Login</h2>
+                <form action="#" method="POST">
+                    <div class="form-input">
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" id="email" required>
+                    </div>
+                    <div class="form-input">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+                    <div class="form-input">
+                        <input type="submit" name="register" value="Register">
+                    </div>
+                </form>
 
 <?php require_once("components/page-end.php"); ?>
