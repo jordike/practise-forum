859ec4a59e03811a50da6ac4eb667840d4fa3982
Jordi Keijzers <jordi.keijzers@rocdeleijgraaf.nl>
2021-03-31 10:35:51 +0200

    Basic site layout * Config file * Home file * Start and end components (header and footer)

---
+++ components/page-start.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title><?php echo $site_name; ?></title>
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php require_once("components/page-start.php"); ?>
+<?php require_once("components/page-end.php"); ?>
