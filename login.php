a4eb57eaad45a99989e0b4a1a2c7cf2aaa6fa3b1
Jordi Keijzers <jordi.keijzers@rocdeleijgraaf.nl>
2021-04-06 12:43:27 +0200

    Basic login system

--- login.php
+++ login.php
@@ -3,6 +3,33 @@
 
     require_once("_config.php");
     require_once("components/page-start.php");
+
+    require_once("db.php");
+
+    if (isset($_POST["login"])) {
+        $query = $db->prepare("SELECT * FROM accounts WHERE email = :email AND password = :password");
+
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
+        $password = sha1(filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING));
+
+        $query->bindParam("email", $email);
+        $query->bindParam("password", $password);
+
+        try {
+            if (!$query->execute()) {
+                echo "interne fout";
+            }
+        } catch (PDOException $exception) {
+            echo $exception->getMessage();
+        }
+
+        // TODO: Betere fout afhandeling.
+        if ($query->rowCount() == 1) {
+            echo "login";
+        } else {
+            echo 'fout';
+        }
+    }
 ?>
 
                 <h2>Login</h2>
@@ -16,7 +43,7 @@
                         <input type="password" name="password" id="password" required>
                     </div>
                     <div class="form-input">
-                        <input type="submit" name="register" value="Register">
+                        <input type="submit" name="login" value="Login">
                     </div>
                 </form>
 
